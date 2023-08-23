7b8a41f4928950a3fadb828b7fedd6615c6b22d0
claudiobzrr <39713988+ClaudioBzrr@users.noreply.github.com>
2023-08-23 14:40:16 +0000

    ✔️ Created:  new socket connetion

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,14 @@
 import Express from 'express';
+import { Server, Socket } from 'socket.io';
 import 'dotenv/config';
+import { createServer } from 'http';
 
 const port = String(process.env.SERVER_PORT) || String(process.env.PORT);
-const server = Express();
+const app = Express();
+const server = createServer(app);
+const io = new Server(server);
 
 server.listen(port, () => console.log(`Server running on port ${port}`));
+io.on('connection', (socket: Socket) =>
+  console.log(`New connection : ${socket.id}`),
+);
